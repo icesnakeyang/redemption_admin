09c687c6fc9ad8424a94b99b394b9c65fe1d2948
liuyang <flow_ocean@yahoo.com>
2022-11-07 14:40:11 +0800

    progress

--- src/api/Api.tsx
+++ src/api/Api.tsx
@@ -1,7 +1,7 @@
 import {Get, Post} from "./ApiBase";
 
-const host = "http://localhost:8004/redemption_api";
-// const host = "http://181.215.246.126:8004/redemption_api";
+// const host = "http://localhost:8004/redemption_api";
+const host = "http://181.215.246.126:8004/redemption_api";
 
 export const apiAdminLogin = (params: any) => {
     return Post(`${host}/admin/admin/adminLogin`, params);
